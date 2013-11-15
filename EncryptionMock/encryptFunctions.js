30e413f89121946678117514f675961ee1846277
Brent Barker <brentbarker9@gmail.com>
2013-11-14 22:19:05 -0700

    Got button listeners to work. Needed to put the script tag at the bottom of the page...

--- EncryptionMock/encryptFunctions.js
+++ EncryptionMock/encryptFunctions.js
@@ -1,10 +1,51 @@
 function encrypt()
 {
-	var textArea = document.getElementById("encryptMessage");
-	console.log("Encrypt stuff");// + textArea.innerHTML);
-	textArea.innerHTML = "hi";//escapeAll(textArea.innerHTML);
+	var textArea = window.document.getElementById("encryptMessage");
+	log("Encrypt stuff");// + textArea.innerHTML);
+	//basic URL encoding... 
+	textArea.innerHTML = escape(textArea.innerHTML);
 }
+
+function decrypt()
+{
+	log("Decrypt method");
+	var textArea = window.document.getElementById("encryptMessage");
+	//basic URL encoding... 
+	textArea.innerHTML = decodeURI(textArea.innerHTML);
+}
+
+function log(log)
+{
+	console.log(log);
+}
+
+function listen()
+{
+	encrypt;
+	console.log("Listener function init");
+	var encryptButtons = document.getElementsByTagName("button");
+	console.log("Button Elements");
+	for(var i = 0; i < encryptButtons.length; i++)
+	{
+		console.log(encryptButtons[i].innerHTML);
+	}
 	
-document.getElementById("encryptBtn").attachEvent("click", function() { 
-       console.log("Clicked") 
-}, false);
+	//listen for encrypt button click
+	document.getElementById("encryptBtn").addEventListener("click", function() { 
+	   log("Encrypt button has been clicked!");
+	   encrypt();
+	}, false);
+	
+	
+	document.getElementById("decryptBtn").addEventListener("click", function() { 
+	   log("Decrypt button has been clicked!");
+	   decrypt();
+	}, false);
+
+}
+
+
+
+window.onload = listen();
+
+
