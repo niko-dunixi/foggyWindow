38fd7b701ce8a66d0cc2efb7699b813514a27496
Paul Baker <son.of.lysander@gmail.com>
2013-11-27 21:40:34 -0700

    Better comments and added a couple variables

--- EncryptionMockSprintTwo/js/injected.js
+++ EncryptionMockSprintTwo/js/injected.js
@@ -5,8 +5,10 @@ if(top.document != document) { //Stop. We only want to work in the main document
   console.log("Environment safe to continue.");
 }
 
-var panelCreated = false;
-var panel = $();
+var panelCreated = false; //obvious boolean is obvious.
+var panel = $(); //Initialize to empty JQuery object. This is where the panel will be housed
+var personal_rsa_object; //set and access this object for our own personal RSA keys
+var friend_rsa_object; //set and access this for our friends
 
 function togglePanel(){
   console.log("Toggling Panel");
