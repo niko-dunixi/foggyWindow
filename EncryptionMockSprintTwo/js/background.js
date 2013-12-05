9e031ab293aaf7f65aa59f69ae8ea16c1be980a2
Brent Barker <brentbarker9@gmail.com>
2013-12-05 00:47:13 -0700

    More dialog stuff. Private key is now being saved to friend_rsa_object in inject.js so the encryption should be able to happen now. Created a bootstrap_rgdpstks.css that only styls our stuff, but the button theme is not saved. I left comments in injection.html on the regex i used to append a class name to every bootstrap value. Keys are now selectable. animated dialog boxes.. keys are now being saved correctly

--- EncryptionMockSprintTwo/js/background.js
+++ EncryptionMockSprintTwo/js/background.js
@@ -4,21 +4,11 @@ if (typeof initialized == "undefined"){
 	localStorage['friends'] = new Array();
 }
 
-chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
-    console.log(sender.tab ?
-                "from a content script:" + sender.tab.url :
-                "from the extension");
-    if (request.greeting == "hello")
-      sendResponse({farewell: "goodbye"});
-  });
-
 //Chrome listener API. The connection between the injected script and the local storage + vice versa.
 chrome.runtime.onConnect.addListener(function(port) {
       //always inject css into the page
-      chrome.tabs.insertCSS(null, {
-        file: 'css/jquery-ui.css'
-      });
+      chrome.tabs.insertCSS(null, {file: 'css/jquery-ui.css'});
+      chrome.tabs.insertCSS(null, {file: 'css/rg-dialog.css'});
   console.log('recieved message port ' + port.name);    
   
 	port.onMessage.addListener(function(msg) {
--- EncryptionMockSprintTwo/js/dialog.js
+++ EncryptionMockSprintTwo/js/dialog.js
@@ -2,6 +2,7 @@
 //Thank you mr PAEz
 //http://stackoverflow.com/questions/13669762/chrome-extention-using-jquery-in-content-script-resolve-error-when-creating-dial
 
+var dialogEffectDurration = 500;
 
 //adds the html of the dialog to the page. This should be removed at a later time.
 function addBasicDialogHtml()
@@ -20,23 +21,22 @@ function sleep(milliseconds) {
 
 function addNewFriendHtml()
 {
-  var div = $('<div />').attr('title', 'Add a new friend').attr('id', 'dialog-form').css('zIndex', 9009 ).appendTo('body');
+  var div = $('<div />').attr('title', 'Add a new friend').attr('id', 'add-friend-dialog-form').css('zIndex', 9009 ).addClass('rgdpstksDialog').appendTo('body');
   var validatTips = $('<p />').text('Please fill out the entire user form. Note: Email is optional!').addClass("validateTips").appendTo(div);
   
   var form = $('<form />').appendTo(div);
-  var fieldSet = $('<fieldset />').appendTo(form);
   
   //name
-  var name = $('<label />').text('Name').css('display', 'block').appendTo(fieldSet);
-  var nameInput = $('<input />', {type: 'text', name: 'name', id: 'name', 'class': 'text ui-widget-content ui-corner-all'}).text('Enter name').css('display', 'block').appendTo(fieldSet);
+  var name = $('<label />').text('Name').css('display', 'block').appendTo(form);
+  var nameInput = $('<input />', {type: 'text', name: 'name', id: 'name', 'class': 'text ui-widget-content ui-corner-all'}).text('Enter name').css('display', 'block').appendTo(form);
   
   //email
-  var email = $('<label />').text('Email (optional)').css('display', 'block').appendTo(fieldSet);
-  var emailInput = $('<input />', {type: 'text', name: 'email', id: 'email', 'class': 'text ui-widget-content ui-corner-all'}).css('display', 'block').appendTo(fieldSet);
+  var email = $('<label />').text('Email (optional)').css('display', 'block').appendTo(form);
+  var emailInput = $('<input />', {type: 'text', name: 'email', id: 'email', 'class': 'text ui-widget-content ui-corner-all'}).css('display', 'block').appendTo(form);
   
   //publicKey
-  var publicKey = $('<label />').text('Public key of Friend').css('display', 'block').appendTo(fieldSet);
-  var publicKeyTextarea = $('<textarea />', {type: 'text', name: 'publicKey', id: 'publicKey', 'class': 'text ui-widget-content ui-corner-all'}).text('private key').css('display', 'block').appendTo(fieldSet);
+  var publicKey = $('<label />').text('Public key of Friend').css('display', 'block').appendTo(form);
+  var publicKeyTextarea = $('<textarea />', {type: 'text', name: 'publicKey', id: 'publicKey', 'class': 'text ui-widget-content ui-corner-all'}).text('private key').css('display', 'block').appendTo(form);
   
   //allFields = $( [] ).add( nameInput ).add( emailInput ).add( publicKeyTextarea ),
 }
@@ -103,11 +103,19 @@ function initAddFriendForm() {
     
   }
   
-  $('#dialog-form').dialog({
+  $('#add-friend-dialog-form').dialog({
     autoOpen: false,
     height: 450,
     width: 450,
     modal: true,
+    show: {
+        effect: "blind",
+        duration: dialogEffectDurration
+    },
+    hide: {
+        effect: "blind",
+        duration: dialogEffectDurration
+    },
     buttons: {
       "Add new friend": function() {
         var bValid = true;
@@ -117,10 +125,10 @@ function initAddFriendForm() {
         //ex. Name has to be between 3 and 16 digits
         bValid = bValid && checkLength( name, "username", 3, 16, true );
         bValid = bValid && checkLength( email, "email", 6, 80, false );
-        bValid = bValid && checkLength( publicKey, "publicKey", 5, 16, true);
+        //bValid = bValid && checkLength( publicKey, "publicKey", 5, 16, true);
         
         //regex for field validations
-        bValid = bValid && checkRegexp( name, /^[a-z]([0-9a-z_])+$/i, "Username may consist of a-z, 0-9, underscores, begin with a letter.", true);
+        bValid = bValid && checkRegexp( name, /^[a-zA-Z]([0-9a-zA-Z_\s])+$/i, "Username may consist of a-z, 0-9, underscores, begin with a letter.", true);
         // From jquery.validate.js (by joern), contributed by Scott Gonzalez: http://projects.scottsplayground.com/email_address_validation/
         bValid = bValid && checkRegexp( email, /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i, "eg. ui@jquery.com", false);
         
@@ -134,9 +142,13 @@ function initAddFriendForm() {
           
           var newFriendStore = new Array();
           newFriendStore.push({'name': name.val(), 'email': email.val(), 'publicKey': publicKey.val()});
+          
+          //store the new friend
           storeNewFriend(name.val(), email.val(), publicKey.val());
           
+          //destroy the dialog box, including the html
           $( this ).dialog( "close" );
+          $("#add-friend-dialog-form").remove();
         }
       },
       Cancel: function() {
@@ -145,6 +157,7 @@ function initAddFriendForm() {
     },
     close: function() {
       allFields.val( "" ).removeClass( "ui-state-error" );
+      $("#add-friend-dialog-form").remove();
     }
   });
 }
@@ -159,7 +172,13 @@ function storeNewFriend(name, email, publicKey)
   msgPort.onMessage.addListener(function(msg) {
     var friends = msg.keys;
     var newFriendStore = new Array();
-    var friendsJson = JSON.parse(friends);
+    var friendsJson;
+    try {
+      friendsJson = JSON.parse(friends);
+    } catch(e) {
+      console.log("error while parsing: " + e);
+      friendsJson = "";
+    }
     console.log('response: ' + friendsJson);
     
     //add existing friends to list
@@ -182,11 +201,16 @@ function storeNewFriend(name, email, publicKey)
 
 }
 
+function deleteFriends()
+{
+   chrome.runtime.connect({name : 'save_friends'}).postMessage({keys: ""});
+}
+
 function addNewFriendDialog()
 {
   addNewFriendHtml();
   initAddFriendForm();
-  $( "#dialog-form" ).dialog( "open" ).zIndex(8983453543);
+  $( "#add-friend-dialog-form" ).dialog( "open" ).zIndex(8983453543);
   $( ".ui-dialog" ).zIndex(99999);
   $( ".ui-widget-overlay" ).zIndex(99999);
 }
@@ -211,40 +235,65 @@ function loadFriendTable()
     msgPort.postMessage({});
   
   msgPort.onMessage.addListener(function(msg) {
-    var friends = msg.keys;
-    console.log('response: ' + friends);
-    parseFriends(friends);
+    var friendsJson = msg.keys;
+    console.log('response: ' + friendsJson);
+    
+    var friendsParsed = JSON.parse(friendsJson);
+    console.log('parsed friends json ' + friendsParsed);
+    $.each(friendsParsed, function(key, value){
+      console.log("key" + key);
+      console.log("value: " + value);
+      
+      //add table html
+      var tr = $('<tr />');
+      $('<td />').text(value.name).appendTo(tr);
+      $('<td />').text(value.email).appendTo(tr);
+      $('<td />').text(value.publicKey).appendTo(tr);
+      
+      tr.appendTo($('#select-friend-table'));
+      
+      //make table rows selectable
+      $(function() {
+        $( "#select-friend-table tbody" ).selectable({
+          stop: function() {
+            $( "tr.ui-selected td", this ).each(function() {
+              console.log("Selected " + $(this).last().text());
+            });
+            
+            var publicKey = $( "tr.ui-selected td", this ).last().text();
+            var selectedFriendName = $( "tr.ui-selected td", this ).first().text();
+            $( "#select-result" ).text("Selected friend " + selectedFriendName);
+            console.log('publicKey ' + publicKey);
+            console.log('friendName ' + selectedFriendName);
+            
+            $('#rdsSelectedFriend').text(selectedFriendName);
+            friend_rsa_object = publicKey;
+            
+          }
+        });
+      });
+    });
   });
  
 }
 
 function parseFriends(friendsJson)
 {
-  console.log('passed friends json ' + friendsJson);
-  var friendsParsed = JSON.parse(friendsJson);
-  console.log('parsed friends json ' + friendsParsed);
-  $.each(friendsParsed, function(key, value){
-				console.log("key" + key);
-				console.log("value: " + value);
-        var tr = $('<tr />');
-        $('<td />').text(value.name).appendTo(tr);
-        $('<td />').text(value.email).appendTo(tr);
-        $('<td />').text(value.publicKey).appendTo(tr);
-        
-        tr.appendTo($('#select-friend-table'));
-			});
+  
   
 }
 
+
+
 function addSelectFriendHtml()
 {
-  var div = $('<div />').attr('title', 'Select a friend').attr('id', 'select-friend').appendTo('body');
-  var validatTips = $('<p />').text('Select your friend').appendTo(div);
+  var div = $('<div />').attr('title', 'Select a friend').attr('id', 'select-friend').addClass('rgdpstksDialog').appendTo('body');
+  var validatTips = $('<p />').attr('id', 'select-result').text('Select your friend').appendTo(div);
   
   var table = $('<table />').width("100%").attr('id', 'select-friend-table').appendTo(div);
-  var trFriend = $('<th />').width("33%").text("friend").appendTo(table);
-  var trEmail = $('<th />').width("33%").text("email").appendTo(table);
-  var trEmail = $('<th />').width("33%").text("publicKey").appendTo(table);
+  var trFriend = $('<th />').width("33%").text("Friend").appendTo(table);
+  var trEmail = $('<th />').width("33%").text("Email").appendTo(table);
+  var trEmail = $('<th />').width("33%").text("Public Key").appendTo(table);
 }
 
 function initSelectFriendDialog()
@@ -254,6 +303,22 @@ $('#select-friend').dialog({
     height: 450,
     width: 450,
     modal: true,
+    show: {
+        effect: "blind",
+        duration: dialogEffectDurration
+    },
+    hide: {
+        effect: "blind",
+        duration: dialogEffectDurration
+    },
+    buttons: {
+      "Ok": function() {
+        $( this ).dialog( "close" );
+      }
+    },
+    close: function() {
+      $("#select-friend").remove();
+    }
   });
 }
 
@@ -270,5 +335,5 @@ function addSelectFriendDialog()
 //this shouldn't be needed, the dialog should take care of closing its self - bbarker
 function closeFriendDialog()
 {
-  $( '#dialog-form' ).dialog( "close" );
+  $( '#add-friend-dialog-form' ).dialog( "close" );
 }
